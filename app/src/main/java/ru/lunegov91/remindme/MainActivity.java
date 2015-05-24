62edcfafaf35b572b0def655acc4fc5f00d07f7d
Антон <GoDevelopPerm@gmail.com>
2015-05-24 19:41:08 +0500

    Добавление стартового активити в файл манифеста.

---
+++ app/src/main/java/ru/lunegov91/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.lunegov91.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Антон on 24.05.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
